e10d6065a654684f37324dba76b55913fd8f0e0b
Tanvir Hasan Tonmoy <tonmoy.thedeveloper@gmail.com>
2025-09-01 23:36:35 +0600

    Login button functionality adding...

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("External js file connected");
+// Login button functionality
+document.getElementById("login-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("Login button clicked");
+  console.log(e);
+});
