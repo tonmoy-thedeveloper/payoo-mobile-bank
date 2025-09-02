6c2889d1e869f8331ab50705f4822b3d68ed239d
Tanvir Hasan Tonmoy <tonmoy.thedeveloper@gmail.com>
2025-09-02 11:49:24 +0600

    Login functionality added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
+// Valid login data
+const phoneNumber = 01314832124;
+const pinNumber = 6464;
+
 // Login button functionality
 document.getElementById("login-btn").addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("Login button clicked");
-  console.log(e);
+
+  // Accessing login data
+  const mobileNumberValue = document.getElementById("phone-number").value;
+
+  const pinNumberValue = document.getElementById("pin-number").value;
+
+  // Converting login credentials to number
+  const mobileNumberValueConvert = parseInt(mobileNumberValue);
+  const pinNumberValueConvert = parseInt(pinNumberValue);
+
+  // Giving conditional to login
+  if (
+    mobileNumberValueConvert === phoneNumber &&
+    pinNumberValueConvert === pinNumber
+  ) {
+    window.location.href = "../home.html";
+  } else {
+    alert("Please put right credentials for login");
+  }
 });
