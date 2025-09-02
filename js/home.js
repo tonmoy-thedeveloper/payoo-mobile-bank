93217223121570048ce1a4dd1ac3f759162166a3
Tanvir Hasan Tonmoy <tonmoy.thedeveloper@gmail.com>
2025-09-03 02:56:08 +0600

    Feat: Add Money feature added

---
+++ js/home.js
@@ -0,0 +1,54 @@
+// Valid Information
+const validPin = 1234;
+const validAccountNumber = 12345678900;
+
+// Add money functionality
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("Add money button clicked");
+
+    // Accessing all data
+    const bankName = document.getElementById("bank").value;
+    const accountNumber = parseInt(
+      document.getElementById("bank-account-number").value
+    );
+    const amount = parseInt(document.getElementById("amount").value);
+    const pinNumber = parseInt(document.getElementById("pin-number").value);
+
+    // console.log(bankName, accountNumber, amount, pinNumber);
+
+    const availableBalance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+    // console.log(availableBalance);
+    if (accountNumber.length < 11) {
+      alert("Please give valid 11 digit account number");
+      return;
+    }
+    if (accountNumber !== validAccountNumber) {
+      alert("Write valid account number");
+      return;
+    }
+    if (pinNumber !== validPin) {
+      alert("Please Provide valid pin number");
+      return;
+    }
+    const totalAvailableBalance = availableBalance + amount;
+
+    document.getElementById("available-balance").innerText =
+      totalAvailableBalance;
+  });
+
+// Toggling Feature
+
+document.getElementById("add-money").addEventListener("click", function () {
+  console.log("Add money clicker");
+  document.getElementById("cashout-section").style.display = "none";
+  document.getElementById("add-money-section").style.display = "block";
+});
+document.getElementById("cash-out").addEventListener("click", function () {
+  document.getElementById("add-money-section").style.display = "none";
+  document.getElementById("cashout-section").style.display = "block";
+});
